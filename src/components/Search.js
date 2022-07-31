a349bd4daadebfda71a493a309fa9f58546b4dab
Brian Koekkoek <bjkoekkoek@hotmail.com>
2022-07-31 11:17:58 +0200

    Initial search feature implemented

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,10 +3,11 @@ import axios from 'axios';
 
 const Search = () => {
 	const [term, setTerm] = useState('');
+	const [results, setResults] = useState([]);
 
 	useEffect(() => {
 		const search = async () => {
-			await axios.get('https://en.wikipedia.org/w/api.php', {
+			const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
 				params: {
 					action: 'query',
 					list: 'search',
@@ -15,8 +16,22 @@ const Search = () => {
 					srsearch: term,
 				},
 			});
+			setResults(data.query.search);
 		};
-		search();
+		if (term) {
+			search();
+		}
+	}, [term]);
+
+	const renderedResults = results.map(res => {
+		return (
+			<div className='item' key={res.pageid}>
+				<div className='content'>
+					<div className='header'>{res.title}</div>
+					<span dangerouslySetInnerHTML={{ __html: res.snippet }}></span>
+				</div>
+			</div>
+		);
 	});
 
 	return (
@@ -31,6 +46,7 @@ const Search = () => {
 					/>
 				</div>
 			</div>
+			<div className='ui celled list'>{renderedResults}</div>
 		</div>
 	);
 };
