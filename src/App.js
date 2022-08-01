96315a6c8cca563bae495dc0d74392c8d251e025
Brian Koekkoek <bjkoekkoek@hotmail.com>
2022-08-01 14:08:44 +0200

    Initial dropdown file creation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 import './index.css';
 
 const items = [
@@ -22,7 +23,7 @@ class App extends React.Component {
 	render() {
 		return (
 			<div className='container'>
-				<Search />
+				<Dropdown />
 			</div>
 		);
 	}
